6ab88af765f9e184905ab49e59cc022bcacd7bc7
Tulqinboyev-Ziyodulla <ziyodulla464@gmail.com>
2024-06-20 22:57:21 +0500

    while

---
+++ js/app.js
@@ -0,0 +1,204 @@
+{
+    // // 1-masala //
+    // function foo(array) {
+    //     let musbat = 0,
+    //         manfiy = 0;
+    //     for (let i = 0; i < array.length; i++) {
+    //         if (array[i] > 0) {
+    //             musbat += array[i];
+    //         } else {
+    //             manfiy += array[i];
+    //         }
+    //     }
+    //     return `${musbat} ${manfiy}`;
+    // }
+    // console.log(foo([" Musbat ", 8, 3]));
+    // console.log(foo([-8, -5, " Manfiy"]));
+
+
+    // // while // //
+    //  function foo(array) {
+    //     let musbat = 0,
+    //     manfiy = 0;
+    //     let i = 0;
+    //     while (i < array.length) {
+    //         if (array[i] > 0) {
+    //             musbat += array[i]
+    //         }else{
+    //             manfiy += array[1]
+    //         }
+
+    //         i++
+    //     }
+    //     return `${musbat} ${manfiy}`
+    //  }
+    //  console.log(foo([" Musbat ", 8, 3]));
+    //  console.log(foo([-8, -5, " Manfiy"]));
+
+
+    // // do while // //
+    // function foo(array) {
+    //     let musbat = 0, manfiy = 0
+    //     let i = 0
+    //     do {
+    //         if (array[i] > 0) {
+    //             musbat += array[i]
+    //         } else {
+    //             manfiy += array[i]
+    //         }
+    //         i++
+    //     } while (i < array.length)
+    //     return `${musbat} ${manfiy}`
+    // }
+    // console.log(foo([" Musbat ", 8, 3]));
+    // console.log(foo([-8, -5, " Manfiy"]));
+}
+
+{
+    // // 2-masala //
+    // function foo(array) {
+    //     let sum = 0
+    //     for (let i = 0; i <= array; i += 2) {
+    //         sum = sum + i
+    //     }
+    //     return ` juft sonlar yigindisi: ${sum}`
+    // }
+    // console.log(foo(12));
+
+
+    // // while // //
+    //     function foo(array){
+    //         let sum = 0
+    //         let i = 0
+    //         while(i <= array){
+    //             sum = sum + i
+    //             i += 2
+    //         }
+    //         return ` juft sonlar yigindisi: ${sum}`
+    //     }
+    //     console.log(foo(12));
+
+
+    // // do while // //
+    //     function foo(array){
+    //         let sum = 0
+    //         let i = 0
+    //         do{
+    //             sum = sum + i
+    //             i += 2
+    //         }while(i <= array)
+    //             return ` juft sonlar yigindisi: ${sum}`
+    //     }
+    //     console.log(foo(12));
+}
+
+{
+    // // 3-masala //
+    //     function foo(a, b) {
+    //         let sum = 0
+    //         for (let i = a; i <= b; i++) {
+    //             sum = sum + i
+    //         }
+    //         return ` yig'indisi: ${sum}`
+    //     }
+    //     console.log(foo(10, 15));
+
+
+    // // while // // 
+    //     function foo(a, b) {
+    //             let i = a
+    //             let sum = 0
+    //         while (i <= b){
+    //           sum = sum + i
+    //           i++
+
+    //         }
+    //         return ` yig'indisi: ${sum}`
+
+    //     }
+    //     console.log(foo(10, 15));
+
+
+    // // do while // //
+    //     function foo(a, b){
+    //         let sum = 0
+    //         let i = a
+    //         do{
+    //             sum = sum + i
+    //             i++
+    //         }while(i <= b)
+    //         return ` yig'indisi: ${sum}`
+    //     }
+    //     console.log(foo(10, 15));
+}
+
+{
+    // 4-masala // //
+    // function foo(n){
+    //     for(let i = n; i >= 0; i -= 2){
+    //         console.log(i);
+    //     }
+    //     return `tugadi`
+    // }
+    // console.log(foo(15));
+
+
+    // // while // // 
+    //    function foo(n){
+    //     let i = n
+    //     while(i >= 0){
+    //         console.log(i);
+    //         i -= 2
+    //     }
+    //     return "tugadi"
+    //    }
+    //    console.log(foo(15));
+
+
+    // // do while // //
+    //     function foo(n){
+    //         let i = n
+    //         do{
+    //             console.log(i);
+    //             i -= 2
+    //         }while(i >= 0)
+    //             return "tugadi"
+    //     }
+    //     console.log(foo(15));
+}
+
+// // // // // // // // // // // // // // // //
+
+{
+    // // While 1-masala //
+    //     function foo( a, b){
+    //         let i = 0
+    //         while(a > b){
+    //             a -= b
+    //             i++
+    //         }
+    //         return a
+    //     }
+    //     console.log(foo(8, 6));
+}
+
+{
+    // // While 2-masala //
+    // function foo(a, b) {
+    //     let i = 0
+    //     while (a < b) {
+    //         a -= b
+    //         i++
+    //     }
+    //     return i
+    // }
+    // console.log(foo(8, 5));
+}
+
+{
+    // While 3-masala // Qila olmadim //
+}
+
+{
+    //While 4- masala // Qila olmadim //
+}
